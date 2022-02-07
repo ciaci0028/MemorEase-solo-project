57033e4a76761c0e27e63c5e4e0a0ecef57bfb96
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-02-07 14:20:00 -0600

    updated fetch photos and added it to root saga for app

--- server/routes/photos.router.js
+++ server/routes/photos.router.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const {
-  rejectUnauthenticated,
+    rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
 
 const router = express.Router();
+
+// Retrieve photos from database
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+
+    let queryText = `
+    SELECT
+        "imageURL"
+    FROM "photos"
+    JOIN "user"
+        ON "user"."id" = "photos"."userID"
+    WHERE "user"."id" = $1;
+    `;
+
+    let queryParams = [
+        req.params.id
+    ];
+
+    pool.query(queryText, queryParams)
+        .then((results) => {
+            res.send(results.rows)
+        })
+        .catch((error) => {
+            console.log('error getting photos', error);
+        });
+        
+});
--- src/redux/sagas/photos.saga.js
+++ src/redux/sagas/photos.saga.js
@@ -4,9 +4,16 @@ import { put, takeEvery } from 'redux-saga/effects';
 // Saga for retrieving photos
 function* fetchPhotos() {
     try {
-        const response = yield axios.get('/')
+        const response = yield axios.get(`/${action.payload}`)
     }
     catch (error) {
         console.log('photo fetch failed', error);
     }
+};
+
+// make this fetchable by whole app
+function* fetchPhotosSaga () {
+    yield takeEvery('GET_PHOTOS', fetchPhotos);
 }
+
+export default fetchPhotosSaga;
