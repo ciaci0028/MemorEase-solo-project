09ad7f962af87792d09be671befb98df3bfe6bee
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-02-18 15:28:54 -0600

    clickable feature

--- src/components/MemoryFeature/MemoryFeature.jsx
+++ src/components/MemoryFeature/MemoryFeature.jsx
@@ -2,6 +2,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+// Card MUI
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function MemoryFeature () {
     const dispatch = useDispatch();
@@ -21,17 +27,33 @@ function MemoryFeature () {
 
     return (
         <>
-            <p>A Special Memory for You</p>
-            <p>{moment(relativeDate).fromNow()}</p>
-            <p>{memory.description} from {moment(memory.photoDate).format('MMMM Do, YYYY')}</p>
+            <p className='specialMemoryCopy'>A Special Memory for You</p>
             {memory ? 
-            <><img
-                src={memory.imageURL}
-            />
+            <>  
+                <p className="timeAgoCopy">{moment(relativeDate).fromNow()}</p>
+                <Card className="memoryCard" sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                    component="img"
+                    max-height="400"
+                    image={memory.imageURL}
+                    alt={memory.description}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {moment(memory.photoDate).format('MMMM Do, YYYY')}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {memory.description}
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                </Card>
             </>
             :
             <p>No photos to share yet! Upload a new photo <Link to="/upload">here</Link></p>
             }
+            
         </>
     )
 };
